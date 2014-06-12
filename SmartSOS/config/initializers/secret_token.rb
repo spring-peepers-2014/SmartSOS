0cb8012bc27de39a6521a9f46b4f9b449f59f606
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-12 15:10:50 -0400

    Add rails skeleton to master

---
+++ SmartSOS/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmartSOS::Application.config.secret_key_base = '6c4393ffa1400c62d8fdaffb947556e629b741ecd2228b84a4a7defa078650e1424881a5e07eff5c3b37660e243c3ec8543bbe7b003cf8ecbe26fe350d74b76d'
---
+++ SmartSOS/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SmartSOS::Application.config.session_store :cookie_store, key: '_SmartSOS_session'
