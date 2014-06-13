cef30ee815e3873ec0c3c80760766042eb4dd7e0
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-13 12:03:00 -0400

    add template for donor show controller testing

--- spec/controllers/donors_controller_spec.rb
+++ spec/controllers/donors_controller_spec.rb
@@ -7,6 +7,17 @@ descrbe DonorsController do
 		it "renders the :new template"
 	end
 
-
+	describe 'GET #show' do 
+		it "assisngs the requested donor to @donor"
+			donor = FactoryGirl.build(:donor)
+			get :show, id: donor 
+			expect(assigns(:donor)).to eq donor
+		end
+		
+		it "renders the :show template" do 
+			donor = FactoryGirl.build(:donor)
+			get :show, id: donor 
+			expect(response).to render_template :show
+		end
 
 end
