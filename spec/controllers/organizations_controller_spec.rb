4ee53bb069c01436ae48bed29f8b6ff3c6fbd33f
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-13 12:10:58 -0400

    update organziation factory

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -3,7 +3,18 @@ require 'spec_helper'
 describe OrganizationsController do 
 
 	describe 'GET #index' do 
-		it 'populates a list of all the organizations'
+		it 'assigns all organziations to @organizations' do
+			org1 = create(:org)
+			org2 = create(:org)
+			get :index
+			expect(assigns(:orgs)).to match_array([org1, org2])
+		end
+	end
+
+	describe 'GET #new' do 
+	end
+
+	describe 'POST #create' do
 	end
 
 end
