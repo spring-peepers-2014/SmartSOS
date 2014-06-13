73d6703580a1465007ef74ea13e69131b89b37a7
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-13 12:30:23 -0400

    add index, new, and create org controller tests

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -12,9 +12,28 @@ describe OrganizationsController do
 	end
 
 	describe 'GET #new' do 
+		it 'assigns a new Organization to @org' do
+			get :new
+			expect(assigns(:org)).to be_a_new(Organization)
+		end
 	end
 
 	describe 'POST #create' do
+		context 'with VALID attributes' do
+			it 'saves the new organization in the database' do
+				expect {
+					post :create, org: create.attributes_for(:org)
+				}.to change(Organization, :count).by(1)
+			end
+		end
+
+		context 'with INVALID attributes' do
+			it "does NOT save the new organization in the database" do
+				expect {
+					post :create, org: create.attributes_for(:invalid_org)
+				}.to_not change(Organization, :count)
+			end
+		end
 	end
 
 end
