5bce86e110c3eb033b6a654b6b0e15be507cdc31
Pei Xiong Liu <peixiong.liu@gmail.com>
2014-06-13 12:38:41 -0400

    Fix bugs in Organization model

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,7 +5,7 @@
   validates :street, presence: true
   validates :city, presence: true
   validates :state, presence: true
-  validates :integer, presence: true
+  validates :zipcode, presence: true
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
   validates :phone, presence: true
