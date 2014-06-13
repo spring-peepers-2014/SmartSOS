becd4ee81985f1bf10996e41dcb95abec9248d1d
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-13 11:40:45 -0400

    add 20% of donor testing

--- spec/factories/donors.rb
+++ spec/factories/donors.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryGirl.define do 
 	factory :donor do 
-		first_name { Faker::Name.last_name }
+		first_name { Faker::Name.first_name }
 		last_name { Faker::Name.last_name }
 		email { Faker::Internet.email }
 		password { "password" }
