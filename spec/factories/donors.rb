a91155b645124651dd07e92aef66b62ed5687f34
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-13 18:44:18 -0400

    Add passing model tests and configure factory girl to work with bcrypt

--- spec/factories/donors.rb
+++ spec/factories/donors.rb
@@ -2,16 +2,19 @@ require 'faker'
 
 FactoryGirl.define do 
 	factory :donor do 
+		x = Faker::Lorem.word
 		first_name { Faker::Name.first_name }
 		last_name { Faker::Name.last_name }
 		email { Faker::Internet.email }
-		# password { "password" }
+		password { x }
+		password_confirmation { x } 
 	end
 
-	factory :invalid_donor do 
-		first_name { nil}
-		last_name { Faker::Name.last_name }
-		email { Faker::Internet.email }
-		# password { "password" }
-	end
+	# factory :invalid_donor do 
+	# 	first_name { nil}
+	# 	last_name { Faker::Name.last_name }
+	# 	email { Faker::Internet.email }
+	# 	password_digest { Faker::Lorem.word }
+	# end
+
 end
