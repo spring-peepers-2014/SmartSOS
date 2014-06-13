f7492dfd352c1d02c5121bc6c028e7427f82be82
Pei Xiong Liu <peixiong.liu@gmail.com>
2014-06-12 23:02:29 -0400

    add presence and unique validations to Organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,6 +4,7 @@ class Organization < ActiveRecord::Base
   validates :city, presence: true
   validates :state, presence: true
   validates :integer, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
   validates :phone, presence: true
   validates :description, presence: true
