1accaf6d09a9604f2e67f20c2361ba1d39b575de
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-13 12:20:00 -0400

    add all GET actions to donor controller

--- spec/controllers/donors_controller_spec.rb
+++ spec/controllers/donors_controller_spec.rb
@@ -3,9 +3,17 @@ require 'spec_helper'
 descrbe DonorsController do 
 
 	describe 'GET #new' do 
-		it "assigns a new Donor to @donor"
-		it "renders the :new template"
+		it "assigns a new Donor to @donor" do 
+			get :new
+			expect(assigns(:donor)) to be_a_new(Donor)
+		end
+
+		it "renders the :new template" do 
+			get :new
+			expect(:response).to render_template :new
+		end
 	end
+
 
 	describe 'GET #show' do 
 		it "assisngs the requested donor to @donor"
@@ -19,5 +27,24 @@ descrbe DonorsController do
 			get :show, id: donor 
 			expect(response).to render_template :show
 		end
+	end
+
+
+		describe 'GET #edit' do 
+			it "assigns the requested donor to @donor" do 
+				donor = FactoryGirl.build(:donor)
+				get :edit, id: donor 
+				expect(assigns(:donor)).to eq donor
+			end
+
+			it "renders the :edit template" do 
+				donor = FactoryGirl.build(:donor) 
+				get :edit, id: donor
+				expect(response).to render_template :edit
+			end
+		end
+
+
+
 
 end
