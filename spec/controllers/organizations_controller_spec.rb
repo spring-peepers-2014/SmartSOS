93dadc49accb40ed7fb2b93621a72b15c1523a7c
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-13 16:19:50 -0400

    update org controller spec

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -3,7 +3,72 @@ require 'spec_helper'
 describe OrganizationsController do 
 
 	describe 'GET #index' do 
-		it 'populates a list of all the organizations'
+		it 'assigns all organizations to @organizations' do
+			org1 = create(:org)
+			org2 = create(:org)
+			get :index
+			expect(assigns(:orgs)).to match_array([org1, org2])
+		end
+	end
+
+	describe 'GET #show' do
+		it 'assigns the requested organization to @org' do
+			org = create(:org)
+			get :edit, id: org
+			expect(assigns(:org)).to eq org
+		end
+	end
+
+	describe 'GET #new' do 
+		it 'assigns a new organization to @org' do
+			get :new
+			expect(assigns(:org)).to be_a_new(Organization)
+		end 
+	end
+
+	describe 'POST #create' do
+		context 'with VALID attributes' do
+			it 'saves the new organization in the database' do
+				expect {
+					post :create, org: create.attributes_for(:org)
+				}.to change(Organization, :count).by(1)
+			end
+		end
+
+		context 'with INVALID attributes' do
+			it "does NOT save the new organization in the database" do
+				expect {
+					post :create, org: create.attributes_for(:invalid_org)
+				}.to_not change(Organization, :count)
+			end
+		end
+	end
+
+	describe 'GET #edit' do
+		it 'assigns the requested organization to @org' do
+			org = create(:org)
+			get :edit, id: org
+			expect(assigns(:org)).to eq org
+		end
+	end
+
+
+	describe 'PATCH #update' do
+		it 'changes the attributes of an organization' do
+			org = create(:org, name: 'exampleName1')
+			patch :update, id: org, org: attributes_for(:org, name: 'exampleName2' )
+			org.reload
+			expect(org.name).to eq('exampleName2')
+		end
+	end
+
+	describe 'DELETE #destroy' do
+		it 'deletes an organization' do
+			org = create(:org)
+			expect {
+				delete :destroy, id: org			
+			}.to change(Organization, :count).by(-1)
+		end
 	end
 
 end
