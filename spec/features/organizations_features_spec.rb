9c75f5b4f143dceedfeab8c5e7aa937375354273
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-13 11:39:28 -0400

    complete create user feature testing

---
+++ spec/features/organizations_features_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature 'Creating Organizations' do
+	before :each do
+		# visit '/'
+		# click_link 'Register Organization'
+	end
+	
+	scenario 'can register a new organization with VALID attributes' do
+	end
+
+	scenario 'can NOT register a new organization with INVALID attributes' do
+	end
+
+end
