b9144738a478c4461d91dc872997ae905dd606e5
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-13 15:02:47 -0400

    add donor controller testing with all actions except index

--- spec/controllers/donors_controller_spec.rb
+++ spec/controllers/donors_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 descrbe DonorsController do 
 
+
 	describe 'GET #new' do 
 		it "assigns a new Donor to @donor" do 
 			get :new
@@ -72,15 +73,54 @@ descrbe DonorsController do
 			end
 		end
 
-	end
+		describe 'PATCH #update' do 
+			before :each do 
+				@donor = FactoryGirl.create(:donor)
+			end
+
+			context "valid attributes" do 
+				it "locates the requested @donor" do 
+					patch :update, id: @donor, donor: attributes_for(:donor)
+					expect(assigns(:donor)).to eq(@donor)
+				end
+
+				it "changes @donor's attributes" do 
+					patch :update, id: @donor,
+						donor: attributes_for(:donor,
+							first_name: "Payam",
+							last_name: "Pakmanesh",
+							email: "p.Pakmanesh@gmail.com",
+							password: "password")
+						@donor.reload
+						expect(@donor.first_name).to eq("Payam")
+						expect(@donor.first_name).to eq("Pakmanesh")
+				end
+
+				it "redirects to the updated donor" do 
+					patch :update, id: @donor, donor: attributes_for(:donor)
+					expect(response).to redirect_to @donor
+				end
+			end
+		end 
+
+		describe 'POST #destroy' do 
+			before :each do 
+				@donor = FactoryGirl.create(:donor)
+			end
+
+			it "deletes the donor" do 
+				expect{
+					delete :destroy, :id: @donor 
+				}.to change(Donor, :donor).by(-1)
+			end
+
+			it "redirects to home page" do 
+				delete :destroy, id: @donor 
+				expect(response).to redirect_to root_path
+			end
+		end
+
+end 
 
 
 
-
-	describe 'PUT #update' do 
-	end
-
-	describe 'POST #destroy' do 
-	end
-
-end
