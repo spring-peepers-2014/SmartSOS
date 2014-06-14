ffbc9267152556932d06dc9d8c5acb463303eb49
Pei Liu & "Donger <pair+girb0t.wontwon@devbootcamp.com>
2014-06-14 17:36:07 -0400

    Add Request resource and custom routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ SmartSOS::Application.routes.draw do
 	root :to => 'campaigns#show_all'
 	resources :organizations do
 		resources :campaigns
+			resources :requests, except: [:show, :edit, :new]
 	end
 
-	resources :donors, except: :index
+	resources :donors, except: [:index]
+
+	# REQUEST ROUTES
+	get '/organizations/:organization_id/campaigns/:id/make_requests' => 'requests#make_requests', as: :make_requests
+	get '/organizations/:organization_id/campaigns/:id/edit_requests' => 'requests#edit_requests', as: :edit_requests
+
 
 	# SESSION ROUTES
 	get '/sessions/login/organizations' => 'sessions#organizations_new', as: :organizations_login
