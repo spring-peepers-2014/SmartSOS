6dcefc2e02ea406fb850d736a6527772c6ca6e8e
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-13 22:03:14 -0400

    Set up session routes for organizations and donors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+
+	def organizations_new
+	end
+
+	def organizations_create
+	end
+
+	def organizations_destroy
+	end
+
+	def donors_new
+	end
+
+	def donors_create
+	end
+
+	def donors_destroy
+	end
+
 end
