1f2b3f289c9e999425704b9d3afc98ca3ed64064
Pei Liu & "Donger <pair+girb0t.wontwon@devbootcamp.com>
2014-06-14 01:30:30 -0400

    Add campaign controller actions and tests for... -show_all -index -new -create -edit -update

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,21 +1,38 @@
 class CampaignsController < ApplicationController
 
 	def show_all
+		@campaigns = Campaign.all
 	end
 
 	def index
+		@organization_campaigns = Organization.find(params[:organization_id]).campaigns
 	end
 
 	def new
+		@campaign = Campaign.new
 	end
 
 	def create
+		campaign = Campaign.new(campaign_params)
+
+		if campaign.save
+			redirect_to organization_campaign_path(params[:organization_id], campaign)
+		else
+			flash[:alert] = "Problem creating new campaign."
+			render 'new'
+		end
 	end
 
 	def edit
+		@campaign = Campaign.find(params[:id])
 	end
 
 	def update
+		p params
+		@campaign = Campaign.find(params[:id])
+		@campaign.update_attributes(campaign_params)
+		p @campaign.errors.full_messages
+		redirect_to organization_campaign_path
 	end
 
 	def show
@@ -23,5 +40,11 @@ class CampaignsController < ApplicationController
 
 	def destroy
 	end
-	
+
+	private
+
+	def campaign_params
+		params.require(:campaign).permit(:organization_id, :name, :description, :start_date, :end_date)
+	end
+
 end
--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -1,9 +1,85 @@
 require 'spec_helper'
 
-describe CampaignsController do 
+describe CampaignsController do
+  let(:campaign) { create :campaign }
+# let!(:organization) { create :organization }
 
-	describe 'GET #index' do 
-		it 'populates a list of all the campaigns'
-	end
+describe 'GET #index' do
+  it 'assigns all campaigns of the organization to @organization_campaigns' do
+      # campaign = create(:campaign)
+      get :index, organization_id: campaign.organization.id
+      expect(assigns(:organization_campaigns)).to match_array([campaign])
+    end
+
+    it 'renders campaigns index page' do
+      # campaign = create(:campaign)
+      get :index, organization_id: campaign.organization.id
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show_all' do
+    it 'assigns all campaigns to @campaigns' do
+      # campaign1 = create(:campaign)
+      campaign2 = create(:campaign)
+      get :show_all
+      expect(assigns(:campaigns)).to match_array([campaign, campaign2])
+    end
+
+    it 'renders campaigns show_all page' do
+      get :show_all
+      expect(response).to render_template :show_all
+    end
+  end
+
+  describe 'GET #new' do
+    it 'assign a new instance of Campaign to @campaign' do
+      get :new, organization_id: campaign.organization_id
+      expect(assigns(:campaign)).to be_a Campaign
+    end
+  end
+
+  describe "POST #create" do
+    let!(:campaign) { create :campaign }
+
+    it 'with valid attributes' do
+      expect{
+        post :create, organization_id: campaign.organization_id, campaign: attributes_for(:campaign)
+        }.to change(Campaign, :count).by(1)
+    end
+
+    it 'with invalid attributes' do
+      expect{
+        post :create, organization_id: campaign.organization_id, campaign: attributes_for(:invalid_campaign)
+        }.to_not change(Campaign, :count)
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'assigns @campaign to requested record of Campaign' do
+      get :edit, organization_id: campaign.organization.id, id: campaign.id
+      expect(assigns(:campaign)).to eq campaign
+    end
+
+    it 'renders edit page' do
+      get :edit, organization_id: campaign.organization.id, id: campaign.id
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #update' do
+    it 'with valid attributes'
+    #   post :update, organization_id: campaign.organization.id,
+    #     id: campaign.id, campaign: { name: "new name",
+    #                                   description: "new description",
+    #                                   start_date: '11-11-2011',
+    #                                   end_date: '12-12-2012' }
+    #   # expect(response).to redirect_to organization_campaign_path(campaign.organization.id, campaign.id)
+    #   # expect(response).to be_success
+    # end
+
+    it 'with invalid attributes'
+  end
+
 
 end
