548882e9fdb57f3acee83404a4b6350e290f9dae
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-14 02:46:53 -0400

    add show view for org

--- spec/features/organizations_features_spec.rb
+++ spec/features/organizations_features_spec.rb
@@ -7,40 +7,40 @@ feature 'Creating Organizations' do
 	end
 	
 	scenario 'can register a new organization with VALID attributes' do
-		# fill_in 'Name', with: 'ExampleOrg'
-		# fill_in 'Description', with: 'this is an example'
-		# fill_in 'Address', with: '1111 Organization Ave'
-		# fill_in 'City', with: 'ImagineLand'
-		# fill_in 'State',with: 'FarAway'
-		# fill_in 'Zip', with: '10011'
-		# fill_in 'Phone', with:'1234567890'
-		# fill_in	'Email', with: 'org@example.com'
-		# fill_in 'Password', with: 'password'
-		# fill_in 'Password Confirmation', with: 'password'
-		# fill_in 'Website URL', with: 'orgexample.com'
-		# click_button 'Register Organization'
+		fill_in 'Name', with: 'ExampleOrg'
+		fill_in 'Description', with: 'this is an example'
+		fill_in 'Address', with: '1111 Organization Ave'
+		fill_in 'City', with: 'ImagineLand'
+		fill_in 'State',with: 'FarAway'
+		fill_in 'Zip', with: '10011'
+		fill_in 'Phone', with:'1234567890'
+		fill_in	'Email', with: 'org@example.com'
+		fill_in 'Password', with: 'password'
+		fill_in 'Password Confirmation', with: 'password'
+		fill_in 'Website URL', with: 'orgexample.com'
+		click_button 'Register Organization'
 
-		# org = Organization.where(name: 'ExampleOrg').first
-		# puts "WHAT IN THE WORLD: #{org}" 
+		org = Organization.where(name: 'ExampleOrg').first
+		puts "WHAT IN THE WORLD: #{org}" 
 
-		# expect(page.current_url).to eq(organization_url(org))
+		expect(page.current_url).to eq(organization_url(org))
 	end
 
 	scenario 'can NOT register a new organization with INVALID attributes' do
-		# fill_in 'Name', with: ''
-		# fill_in 'Description', with: 'this is an example'
-		# fill_in 'Address', with: '1111 Organization Ave'
-		# fill_in 'City', with: 'ImagineLand'
-		# fill_in 'State',with: 'FarAway'
-		# fill_in 'Zip', with: '10011'
-		# fill_in 'Phone', with: '1234567890'
-		# fill_in	'Email', with: 'org@example.com'
-		# fill_in 'Password', with: 'password'
-		# fill_in 'Password Confirmation', with: 'password'
-		# fill_in 'Website URL', with: 'orgexample.com'
-		# click_button 'Register Organization'
+		fill_in 'Name', with: ''
+		fill_in 'Description', with: 'this is an example'
+		fill_in 'Address', with: '1111 Organization Ave'
+		fill_in 'City', with: 'ImagineLand'
+		fill_in 'State',with: 'FarAway'
+		fill_in 'Zip', with: '10011'
+		fill_in 'Phone', with: '1234567890'
+		fill_in	'Email', with: 'org@example.com'
+		fill_in 'Password', with: 'password'
+		fill_in 'Password Confirmation', with: 'password'
+		fill_in 'Website URL', with: 'orgexample.com'
+		click_button 'Register Organization'
 
-		# expect(page).to have_content('Apologies. Your Organization has not been registered.')
+		expect(page).to have_content('Apologies. Your Organization has not been registered.')
 	end
 
 end
@@ -52,23 +52,23 @@ feature 'Editing Organizations' do
 	end
 
 	describe 'Organization Administrators' do
-		# scenario 'can edit an existing organization' do
-		# 	visit '/'
-		# 	click_link 'Log In Organization'
-		# 	fill_in 'Name', with: 'Example'
-		# 	fill_in 'Password', with: 'password'
-		# 	click_button 'Login'
-		# 	expect(page).to have_content 'Update Organization'
-		# end
+		scenario 'can edit an existing organization' do
+			visit '/'
+			click_link 'Log In Organization'
+			fill_in 'Name', with: 'Example'
+			fill_in 'Password', with: 'password'
+			click_button 'Login'
+			expect(page).to have_content 'Update Organization'
+		end
 	end
 
 	describe 'Donors' do
-		# scenario 'can NOT edit an existing organization' do
-		# 	visit organization(@org)
-		# 	scenario 'can NOT edit an existing organization' do
-		# 		expect(page).to have_no_content 'Update Organization'
-		# 	end
-		# end
+		scenario 'can NOT edit an existing organization' do
+			visit organization(@org)
+			scenario 'can NOT edit an existing organization' do
+				expect(page).to have_no_content 'Update Organization'
+			end
+		end
 	end
 
 end
