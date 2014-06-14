563d75a2d7f9f5bda96549ade54662d938f726db
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-14 13:37:10 -0400

    implement sessions contoller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,22 +9,43 @@ class SessionsController < ApplicationController
 				session[:organization_id] = organization.id
 				redirect_to organization_path(organization)
 			else
-				flash.now.alert = "Invalid Password"
-				render "new"
+				flash.now.alert = 'Invalid Password'
+				render 'new_organization'
 			end
+		else
+			flash.now.alert = 'Invalid Organization Name'
+			render 'new_organization'
 		end
 	end
 
 	def organizations_destroy
+		reset_session
+		flash[:alert] = 'You have successfully logged out.'
+		redirect_to root_path
 	end
 
 	def donors_new
 	end
 
 	def donors_create
+		if doner = Doner.find_by_name(params[:email])
+			if donor.authenticate(params[:password])
+				session[:donor_id] = donor.id
+				redirect_to donor_path(donor)
+			else
+				flash.now.alert = 'Invalid Password'
+				render 'new_donor'
+			end
+		else
+			flash.now.alert = 'Invalid Email'
+			render 'new_donor'
+		end
 	end
 
 	def donors_destroy
+		reset_session
+		flash[:alert] = 'You have successfully logged out.'
+		redirect_to root_path
 	end
 
 end
