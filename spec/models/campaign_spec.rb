f96b00baa36e5d1cf157d2a7642b766487d52908
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-15 10:07:07 -0400

    Adding first few tests to campaign model

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe Campaign do
-  skip "add some examples to (or delete) #{__FILE__}"
+	context "it should belong to organization" do
+		it { should belong_to(:organization) }
+	end
+
+	context "it should have many requests" do
+		it { should have_many(:requests) }
+	end
+
+	context "it should have many requested_items" do
+		it { should have_many(:requested_items).through(:requests) }
+	end
+
+	context "it should have many pledges" do
+		it { should have_many(:pledges) }
+	end
+
+	# context "it should have many requests" do
+	# 	it { should have_many(:requests) }
+	# end
+
+	# context "it should have many requests" do
+	# 	it { should have_many(:requests) }
+	# end
+
 end
+
+
+  # belongs_to :organization
+  # has_many :requests
+  # has_many :requested_items, through: :requests, source: :item
+  # has_many :pledges
+  # has_many :pledged_items, through: :pledges, source: :pledged_item
+  # has_many :donors, through: :pledges
