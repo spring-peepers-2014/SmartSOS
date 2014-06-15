c22d48df80ca30801ba48dc91e2f6247516445d8
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-15 09:52:29 -0400

    Fix all pledge model tests

--- spec/models/pledge_spec.rb
+++ spec/models/pledge_spec.rb
@@ -13,26 +13,4 @@ describe Pledge do
 		it { should belong_to(:pledged_item) }
 	end
 
-
-
 end
-
-
-# describe Donor do 
-# 		it "is valid with a first name, lastname, email and password" do 
-# 		donor = FactoryGirl.build(:donor)
-# 		expect(donor).to be_valid
-# 	end
-
-# 	context "it should have many pledges" do
-# 		it { should have_many(:pledges) }
-# 	end
-
-# 	context "it should respond to items" do
-# 		it { should have_many(:pledged_items).through(:pledges) }
-# 	end
-
-# 	context "it should respond to campaigns" do
-# 		it { should have_many(:campaigns).through(:pledges) }
-# 	end
-# end
