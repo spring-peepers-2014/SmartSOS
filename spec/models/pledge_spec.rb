a98bd21517be84090a99c8b2d1047b19c4d3636c
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-15 09:47:29 -0400

    Add third model test for pledge

--- spec/models/pledge_spec.rb
+++ spec/models/pledge_spec.rb
@@ -9,26 +9,30 @@ describe Pledge do
 		it { should belong_to(:campaign) }
 	end
 
+	context "it should belong to item" do
+		it { should belong_to(:pledged_item) }
+	end
+
 
 
 end
 
 
-describe Donor do 
-		it "is valid with a first name, lastname, email and password" do 
-		donor = FactoryGirl.build(:donor)
-		expect(donor).to be_valid
-	end
+# describe Donor do 
+# 		it "is valid with a first name, lastname, email and password" do 
+# 		donor = FactoryGirl.build(:donor)
+# 		expect(donor).to be_valid
+# 	end
 
-	context "it should have many pledges" do
-		it { should have_many(:pledges) }
-	end
+# 	context "it should have many pledges" do
+# 		it { should have_many(:pledges) }
+# 	end
 
-	context "it should respond to items" do
-		it { should have_many(:pledged_items).through(:pledges) }
-	end
+# 	context "it should respond to items" do
+# 		it { should have_many(:pledged_items).through(:pledges) }
+# 	end
 
-	context "it should respond to campaigns" do
-		it { should have_many(:campaigns).through(:pledges) }
-	end
-end
+# 	context "it should respond to campaigns" do
+# 		it { should have_many(:campaigns).through(:pledges) }
+# 	end
+# end
