4238d84b892eccf101839aba1a50a1cd3086bd47
Tuan Duong <tuanduong15@gmail.com>
2014-06-16 18:28:45 -0400

    Remove end_date validation in model and strong params

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -9,8 +9,6 @@ class Campaign < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true
   validates_length_of :description, maximum: 500
-  validates :end_date, presence: true
-  validates_date :end_date
 
   def total_requests_price
     requests = self.requests
