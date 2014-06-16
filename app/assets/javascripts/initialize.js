4f88c4efe31619783898676f7cbdf4618123c246
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-15 20:36:16 -0400

    Fix bugs and add JS file

---
+++ app/assets/javascripts/initialize.js
@@ -0,0 +1,14 @@
+$(function () {
+	$('#logo').hide().fadeIn(3000);                
+	$('#S').hide().fadeIn(4000);                
+	$('#O').hide().fadeIn(5000);                
+	$('#S_2').hide().fadeIn(6000);                
+	$('#button').hide().fadeIn(3000);                
+	$('#quote').hide().fadeIn(7500);    
+	$('#nav').hide().fadeIn(2000);    
+
+	function open_win() {
+		alert('Open new tab to Amazon?')
+	}
+
+}); 
