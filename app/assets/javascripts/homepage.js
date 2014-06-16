6394f292cae316b461f18fdac75dd720a6e8a361
Alexander Frankel <alexxander.frankel@gmail.com>
2014-06-15 21:49:46 -0400

    update homepage formatting

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,9 +1,9 @@
-function animateHomepage() {	
-	$('#logo').hide().fadeIn(700);                
-	$('#S').hide().fadeIn(1000);                
-	$('#O').hide().fadeIn(1300);                
-	$('#S_2').hide().fadeIn(1600);                
-	$('#button').hide().fadeIn(700);                
-	$('#quote').hide().fadeIn(2500);    
-	$('#nav').hide().fadeIn(500); 
+function animateHomepage() {
+	$('#logo').hide().fadeIn(700);
+	$('#S').hide().fadeIn(1000);
+	$('#O').hide().fadeIn(1300);
+	$('#S_2').hide().fadeIn(1600);
+	$('#button').hide().fadeIn(700);
+	$('#quote').hide().fadeIn(1000);
+	$('#nav').hide().fadeIn(500);
 }
